bf7cfeb236d1e37a0b1a2859c1a113b5fea7290d
Trey Amador <trey.amador@appfolio.com>
2019-03-07 23:57:36 -0800

    simplify main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
     passage = 'That which is, or that which was, of our little fixations which encumber us '\
       'and shake us unquestioningly. They shake us to so many cores and centers '\
       'like so many of the grotesques and the little rivulets of fortune and thought. '
-    @text = passage * 6
+    @text = passage * 15
   end
 end
