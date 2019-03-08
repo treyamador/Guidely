62683b5d7696c6766b219e15f18d95f370d0fee8
Trey Amador <trey.amador@appfolio.com>
2019-03-07 22:24:31 -0800

    highlighting corresponds with keypress event

--- app/javascript/components/Page.js
+++ app/javascript/components/Page.js
@@ -2,28 +2,40 @@ import React from "react"
 import PropTypes from "prop-types"
 import Word from './Word'
 
+const COLOR_MAP = [
+  '#F2D7D5', '#F7DC6F', '#7DCEA0', '#85C1E9', '#BB8FCE',
+  '#F1948A', '#F8C471', '#FCF3CF', '#D4EFDF', '#D6EAF8'
+];
+
 class Page extends React.Component {
   constructor(props) {
     super(props);
+    this.keyListener();
     this.state = {
       text: this.parse(this.props.text)
     };
   }
 
-  parse = (text) => {
-    return text.split(/(\s+)/).map(
-      (word, index) => <Word key={'word-'+index}>{ word }</Word>
-    );
+  keyListener = () => {
+    this.keymap = new Map();
+    document.addEventListener('keydown', (evnt) => {
+      this.keymap[evnt.which] = true;
+    });
+    document.addEventListener('keyup', (evnt) => {
+      delete this.keymap[evnt.which];
+    });
   };
 
-  display = () => {
-
-  }
+  parse = (text) => {
+    return text.split(/(\s+)/).map(
+      (word, index) => <Word key={'word-'+index} keymap={ this.keymap }>{ word }</Word>
+    );
+  };
 
   render () {
     return (
       <div className="page__wrapper">
-        <div className="page">
+        <div className="page" >
           <div className="page__text">
             { this.state.text }
           </div>
--- app/javascript/components/Word.js
+++ app/javascript/components/Word.js
@@ -1,10 +1,34 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+const ZERO_KEY = 48;
+const DIGITS = 10;
+
 class Word extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      highlight: 'transparent'
+    };
+  }
+
+  highlight = (evnt) => {
+    this.setState({
+      highlight: 'yellow'
+    });
+  };
+
   render () {
+    let wordStyle = {
+      backgroundColor: this.state.highlight
+    }
+
     return (
-      <span className="word">
+      <span className="word"
+            style={ wordStyle }
+            onMouseOver={ this.highlight }
+            onKeyDown={ this.keydown }
+            onKeyUp={ this.keyup } >
         { this.props.children }
       </span>
     );
